e478434e760947892c0faf427e439663973ab9d5
DasKaroWow <daskarowow@gmail.com>
2026-03-30 19:49:05 +0300

    feat: expose subpackages in public package API

--- src/singbox_models/__init__.py
+++ src/singbox_models/__init__.py
@@ -1,3 +1,28 @@
 from .config import Config
 
-__all__ = ["Config"]
+from . import base
+from . import certificate
+from . import dns
+from . import endpoint
+from . import experimental
+from . import inbound
+from . import log
+from . import ntp
+from . import outbound
+from . import route
+from . import service
+
+__all__ = [
+    "Config",
+    "base",
+    "certificate",
+    "dns",
+    "endpoint",
+    "experimental",
+    "inbound",
+    "log",
+    "ntp",
+    "outbound",
+    "route",
+    "service",
+]
